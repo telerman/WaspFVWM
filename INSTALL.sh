f941ec256d858bab47201b52df4e3fa5f5a88ed1
telerman <oleg.telerman@gmail.com>
2017-03-06 17:37:43 +0200

    INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+backup_dir=env_backup_`date +%Y%m%d%H%M%S`
+echo $backup_dir
+mkdir ${HOME}/${backup_dir}
+
+make_backup ()
+{
+    if [ -e "$1" ]
+    then
+	mv -v "$1" ${HOME}/${backup_dir}/
+    fi
+}
+
+make_backup ${HOME}/.Xresources
+make_backup ${HOME}/.emacs
+make_backup ${HOME}/.fvwm
+make_backup ${HOME}/.conkyrc
+
+mv -v ./.Xresources ${HOME}/.Xresources
+mv -v ./.emacs ${HOME}/.emacs
+mv -v ./.conkyrc ${HOME}/.conkyrc
+
+
