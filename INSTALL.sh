82cb6af561a83586979ab1fc74139be61d3c3232
telerman <oleg.telerman@gmail.com>
2017-06-28 21:35:37 +0300

    add fonts

--- INSTALL.sh
+++ INSTALL.sh
@@ -62,6 +62,9 @@ fi
 
 wget https://fontlibrary.org/assets/downloads/lcd/0bbb50971deb300331139c65b16b37c8/lcd.zip -O ${DESTINATION}/.fonts/lcd.zip
 wget https://fontlibrary.org/assets/downloads/xolonium/e00c124f3e1b130e5ec2a7eee2f4f1b8/xolonium.zip -O ${DESTINATION}/.fonts/xolonium.zip
+wget https://fontlibrary.org/assets/downloads/pfennig/8b5fa73ca4cf4cfa42d21b9f73f6060b/pfennig.zip -O ${DESTINATION}/.fonts/pfennig.zip
+wget https://fontlibrary.org/assets/downloads/libertinus-sans/1123c9f83b4e5c15919633f318094b1c/libertinus-sans.zip -O ${DESTINATION}/.fonts/libertinus-sans.zip
+wget https://fontlibrary.org/assets/downloads/liberation-sans/2b246ab94ea322ca5282dfd1a39c36ec/liberation-sans.zip -O ${DESTINATION}/.fonts/liberation-sans.zip
 
 for i in ${DESTINATION}/.fonts/*.zip
 do
